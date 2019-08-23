ed268964078fd48f42488d5cf2ab98b9b1f779a9
GuHenry <guga_hmaia@outlook.com>
2019-08-23 11:05:00 -0300

    Criado classes do banco

--- Model/Historico.cs
+++ Model/Historico.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Model
 {
-    class Historico
+    public class Historico
     {
     }
 }
--- Model/MovimentacaoFinanceiraSaida.cs
+++ Model/MovimentacaoFinanceiraSaida.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Model
 {
+    [Table("movimentacao_financeira_saida")]
     public class MovimentacaoFinanceiraSaida
     {
+        [Key,Column("id")]
+        public int Id { get; set; }
+
+        #region fk_contas_corrente
+        [Column("IdContasCorrente")]
+        public int IdContasCorrente { get; set; }
+
+        #endregion
     }
 }
