52efe3461bd17c46759f23090ae6b721a120b363
Paulo-Schaffer <51707330+Paulo-Schaffer@users.noreply.github.com>
2019-08-23 09:21:31 -0300

    Commit inicial

---
+++ Model/ClientePessoaFisica.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    [Table("clientes_pessoa_fisica")]
+    public class ClientePessoaFisica
+    {
+        [Key, Column("id")]
+        public int Id { get; set; }
+
+        [Column("nome")]
+        public string Nome { get; set; }
+
+        [Column("cpf")]
+        public string Cpf { get; set; }
+
+        [Column("data_vencimento")]
+        public DateTime DataVencimento { get; set; }
+
+        [Column("limite_credito")]
+        public decimal LimiteCredito { get; set; }
+
+        [Column("emal")]
+        public string Email { get; set; }
+
+        [Column("telefone")]
+        public int Telefone { get; set; }
+
+        [Column("cep")]
+        public string Cep { get; set; }
+
+        [Column("numero")]
+        public int Numero { get; set; }
+
+        [Column("bairro")]
+        public string Bairro { get; set; }
+
+        [Column("cidade")]
+        public string Cidade { get; set; }
+
+        [Column("uf")]
+        public string Uf { get; set; }
+
+        [Column("complemento")]
+        public string Complemento { get; set; }
+    }
+}
---
+++ Model/ClientePessoaJuridica.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    [Table("clientes_pessoa_juridica")]
+    public class ClientePessoaJuridica
+    {
+       
+
+        [Key, Column("id")]
+        public int Id { get; set; }
+
+        [Column("razao_social")]
+        public string RazaoSocial { get; set; }
+
+        [Column("atividade")]
+        public string Atividade { get; set; }
+
+        [Column("nome_fantasia")]
+        public string NomeFantasia { get; set; }
+
+        [Column("data_cadastro")]
+        public DateTime DataCadastro { get; set; }
+
+        [Column("cnpj")]
+        public string Cnpj { get; set; }
+
+        [Column("email")]
+        public string Email { get; set; }
+
+        [Column("filial")]
+        public string Filial { get; set; }
+
+        [Column("telefone")]
+        public string Telefone { get; set; }
+
+        [Column("cep")]
+        public string Cep { get; set; }
+
+        [Column("logradouro")]
+        public string Logradouro { get; set; }
+
+        [Column("numero")]
+        public int Numero { get; set; }
+
+        [Column("bairro")]
+        public string Bairro { get; set; }
+
+        [Column("uf")]
+        public string Uf { get; set; }
+
+        [Column("cidade")]
+        public string Cidade { get; set; }
+    }
+}
