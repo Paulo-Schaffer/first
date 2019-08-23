b92783668d18079221d3f53583e783fdfb6c1d8f
AndreWenceslau <andrewenceslaudeoli@gmail.com>
2019-08-23 11:19:31 -0300

    model: categoria despesas, movimentações dispesas entrada, e titulos a receber

--- Model/CategoriaDespesas.cs
+++ Model/CategoriaDespesas.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,14 @@ namespace Model
 {
     public class CategoriaDespesas
     {
+        [Table("categorias_despesas")]
+        public class CatgoriasDespesas
+        {
+            [Key, Column("id")]
+            public int Id { get; set; }
+
+            [Column("tipo_despesa")]
+            public string TipoDespesa { get; set; }
+        }
     }
 }
--- Model/MovimentacaoFinanceiraEntrada.cs
+++ Model/MovimentacaoFinanceiraEntrada.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,39 @@ namespace Model
 {
     class MovimentacaoFinanceiraEntrada
     {
+        [Table("movimentacoes_financeiras_entrada")]
+        public class MovimentacoesFinanceirasEntrada
+        {
+            [Key, Column("id")]
+            public int Id { get; set; }
+
+            [Column("valor")]
+            public decimal Valor { get; set; }
+
+            #region key_conta_corrente
+            [Column("id_conta_corrente")]
+            public int IdContaCorrente { get; set; }
+
+            [ForeignKey("IdContaCorrente")]
+            public ContaCorrente ContaCorrente { get; set; }
+            #endregion
+
+            #region key_id_caixa
+            [Column("id_caixa")]
+            public int IdCaixa { get; set; }
+
+            [ForeignKey("IdCaixa")]
+            public Caixa Caixa { get; set; }
+            #endregion
+
+            #region key_parcela_receber
+            [Column("id_parcela_receber")]
+            public int  IdParecelaReceber { get; set; }
+
+            [ForeignKey("IdParcelaReceber")]
+            public ParcelaReceber ParcelaReceber { get; set; }
+            #endregion
+
+        }
     }
 }
