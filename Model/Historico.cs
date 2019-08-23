1751cc853bb126b94c1e6deebd0214782177da3f
paullew94 <paullew@hotmail.com>
2019-08-23 10:48:55 -0300

    quase pronto as classes

--- Model/Historico.cs
+++ Model/Historico.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Model
 {
-    class Historico
+    [Table("historicos")]
+    public class Historico
     {
+        [Key, Column("id")]
+        public int Id { get; set; }
+
+        [Column("descricao"),StringLength(100)]
+        public string Descricao { get; set; }
+            
     }
 }
