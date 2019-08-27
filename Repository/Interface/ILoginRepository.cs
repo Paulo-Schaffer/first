1edc9000e0239eae16b5a4575ce2f489e886b9bc
JoaoPstein <51447559+JoaoPstein@users.noreply.github.com>
2019-08-27 18:12:40 -0300

    Adicionado interface/repositorio login, funcionario, titulo pagar

---
+++ Repository/Interface/ILoginRepository.cs
@@ -0,0 +1,29 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interfaces
+{
+    interface ILoginRepository
+    {
+        int Inserir(Login login);
+
+        bool Alterar(Login login);
+
+        List<Login> ObterTodos(
+            int id,
+            string usuario,
+            string senha,
+            int id_funcionario
+        );
+
+        Login ObterPeloId(int id);
+
+        bool Apagar(int id);
+
+        List<Login> ObterTodosSelect2(string pesquisa);
+    }
+}
