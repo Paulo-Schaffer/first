cf07028cfa79f61e17f4390a63a7b48464f22529
GuHenry <guga_hmaia@outlook.com>
2019-08-28 08:09:00 -0300

    Adicionado pasta Interface ao projeto

---
+++ TccFirst/Interfaces/ICategoriaDespesaRepository.cs
@@ -0,0 +1,22 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interfaces
+{
+    public interface ICategoriaDespesaRepository
+    {
+        int Inserir(CategoriaDespesa categoriaDespesa);
+
+        bool Alterar(CategoriaDespesa categoriaDespesa);
+
+        List<CategoriaDespesa> ObterTodos();
+
+        bool Apagar(int id);
+
+        CategoriaDespesa ObterPeloId(int id);
+    }
+}
---
+++ TccFirst/Interfaces/ITituloReceberRepository.cs
@@ -0,0 +1,22 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interfaces
+{
+    public interface ITituloReceberRepository
+    {
+        int Inserir(TituloReceber tituloReceber);
+
+        bool Alterar(TituloReceber tituloReceber);
+
+        List<TituloReceber> ObterTodos();
+
+        bool Apagar(int id);
+
+        TituloReceber ObterPeloId(int id);
+    }
+}
