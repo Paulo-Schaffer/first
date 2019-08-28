7d99cf8647edd1b85d4348e229c9f54333b087f4
paullew94 <paullew@hotmail.com>
2019-08-28 09:40:16 -0300

    criado as repositorios de Historico, ParcelaReceber e ClientePessoaJuridica

---
+++ Repository/Repositories/ClientePessoaJuridicaRepository.cs
@@ -0,0 +1,38 @@
+﻿using Model;
+using Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repositories
+{
+    class ClientePessoaJuridicaRepository : IClientePessoaJuridicaRepository
+    {
+        public bool Alterar(ClientePessoaJuridica clientepessoajuridica)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Apagar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Inserir(ClientePessoaJuridica clientepessoajuridica)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClientePessoaJuridica ObterPeloId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ClientePessoaJuridica> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
