dab3779a34db6e8bfaaf947338867c418dd94fa6
GuHenry <guga_hmaia@Outlook.com>
2019-08-29 08:18:55 -0300

    Arrumado sistema context

--- Repository/SistemaContext.cs
+++ Repository/SistemaContext.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,21 +10,14 @@ namespace Repository
 {
     class SistemaContext : DbContext
     {
-        public DbSet<TituloPagar> tituloPagar { get; set; }
-        public DbSet<TituloPagar> tituloPagar { get; set; }
+        public DbSet<TituloPagar> TitulosPagar { get; set; }
 
 
-        public DbSet<TituloReceber> TitulosReceber { get; set; }
         public DbSet<TituloReceber> TitulosReceber { get; set; }
         public DbSet<MovimentacaoFinanceiraEntrada> MovimentacaoFinanceiraEntradas { get; set; }
         public DbSet<CategoriaDespesa> CategoriasDespesas { get; set; }
         public DbSet<Agencia> Agencias { get; set; }
-
         public DbSet<MovimentacaoFinanceiraEntrada> movimentacaoFinanceiraEntradas { get; set; }
-        public DbSet<MovimentacaoFinanceiraEntrada> movimentacaoFinanceiraEntradas { get; set; }
-        public DbSet<CategoriaDespesa> categoriasDespesas { get; set; }
-        public DbSet<CategoriaDespesa> categoriasDespesas { get; set; }
-        public DbSet<ParcelaReceber> ParcelaRecebers { get; set; }
-        public DbSet<ParcelaReceber> ParcelaRecebers { get; set; }
+        public DbSet<ParcelaReceber> ParcelasReceber { get; set; }
     }
 }
