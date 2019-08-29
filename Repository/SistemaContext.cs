de3b0a33cb8fab0625e3a7b74bda9d94051244bf
GuHenry <guga_hmaia@Outlook.com>
2019-08-29 08:06:43 -0300

    Refeito sistema context

---
+++ Repository/SistemaContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    class SistemaContext : DbContext
+    {
+        public DbSet<TituloPagar> tituloPagar { get; set; }
+        public DbSet<TituloPagar> tituloPagar { get; set; }
+
+
+        public DbSet<TituloReceber> TitulosReceber { get; set; }
+        public DbSet<TituloReceber> TitulosReceber { get; set; }
+        public DbSet<MovimentacaoFinanceiraEntrada> MovimentacaoFinanceiraEntradas { get; set; }
+        public DbSet<CategoriaDespesa> CategoriasDespesas { get; set; }
+        public DbSet<Agencia> Agencias { get; set; }
+
+        public DbSet<MovimentacaoFinanceiraEntrada> movimentacaoFinanceiraEntradas { get; set; }
+        public DbSet<MovimentacaoFinanceiraEntrada> movimentacaoFinanceiraEntradas { get; set; }
+        public DbSet<CategoriaDespesa> categoriasDespesas { get; set; }
+        public DbSet<CategoriaDespesa> categoriasDespesas { get; set; }
+        public DbSet<ParcelaReceber> ParcelaRecebers { get; set; }
+        public DbSet<ParcelaReceber> ParcelaRecebers { get; set; }
+    }
+}
