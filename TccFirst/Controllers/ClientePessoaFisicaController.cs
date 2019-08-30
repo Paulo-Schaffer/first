ee0362a2b4c3fca16719d8ee9c54d2a8ccff8b5a
AndreWenceslau <andrewenceslaudeoli@gmail.com>
2019-08-30 11:23:05 -0300

    Cliente pessoa fisica não terminado

---
+++ TccFirst/Controllers/ClientePessoaFisicaController.cs
@@ -0,0 +1,23 @@
+﻿using Repository.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TccFirst.Controllers
+{
+    public class ClientePessoaFisicaController : Controller
+    {
+        private ClientePessoaFisicaRepository repository;
+
+        public ClientePessoaFisicaController()
+        {
+            repository = new ClientePessoaFisicaRepository();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
