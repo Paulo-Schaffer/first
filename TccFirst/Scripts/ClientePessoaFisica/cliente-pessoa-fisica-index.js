0b0efae8d739789f8f12a6c25d16d5d59f917961
AndreWenceslau <andrewenceslaudeoli@gmail.com>
2019-09-04 14:20:56 -0300

    CRUD tabela clientesPessoaFisica Finalizado

--- TccFirst/Scripts/ClientePessoaFisica/cliente-pessoa-fisica-index.js
+++ TccFirst/Scripts/ClientePessoaFisica/cliente-pessoa-fisica-index.js
@@ -70,7 +70,6 @@
                 numero: $numero,
                 bairro: $bairro,
                 cidade: $cidade,
-
                 uf: $uf,
                 complemento: $complemento
 
@@ -79,7 +78,7 @@
             success: function (data) {
                 $("#modal-clientePessoaFisica").modal("hide");
                 $idAlterar = -1;
-                $tabela.ajax.reload();
+                $tabelaClientePessoaFisica.ajax.reload();
             },
             error: function (err) {
                 alert("Não foi possível alterar");
