2ce45b6952ad7fd09232cbb903f1975b06f6d538
paullew94 <paullew@hotmail.com>
2019-09-04 16:27:56 -0300

    alterou botao editar cliente pessoa juridica

--- TccFirst/Scripts/ClientePessoaJuridica/clientePessoaJuridica.js
+++ TccFirst/Scripts/ClientePessoaJuridica/clientePessoaJuridica.js
@@ -132,20 +132,20 @@
             url: "/clientePessoaJuridica/obterpeloid?id=" + $idAlterar,
             method: 'get',
             success: function (data) {
-                $('#clientePessoaJuridica-campo-razaoSocial').val(data.razaoSocial);
-                $('#clientePessoaJuridica-campo-atividade').val(data.atividade);
-                $('#clientePessoaJuridica-campo-nomeFantasia').val(data.nomeFantasia);
-                $('#clientePessoaJuridica-campo-dataCadastro').val(data.dataCadastro);
-                $('#clientePessoaJuridica-campo-cnpj').val(data.cnpj);
-                $('#clientePessoaJuridica-campo-email').val(data.email);
-                $('#clientePessoaJuridica-campo-filial').val(data.filial);
-                $('#clientePessoaJuridica-campo-telefone').val(data.telefone);
-                $('#clientePessoaJuridica-campo-cep').val(data.cep);
-                $('#clientePessoaJuridica-campo-logradouro').val(data.logradouro);
-                $('#clientePessoaJuridica-campo-numero').val(data.numero);
-                $('#clientePessoaJuridica-campo-bairro').val(data.bairro);
-                $('#clientePessoaJuridica-campo-uf').val(data.uf);
-                $('#clientePessoaJuridica-campo-cidade').val(data.cidade);
+                $('#clientePessoaJuridica-campo-razaoSocial').val(data.RazaoSocial);
+                $('#clientePessoaJuridica-campo-atividade').val(data.Atividade);
+                $('#clientePessoaJuridica-campo-nomeFantasia').val(data.NomeFantasia);
+                $('#clientePessoaJuridica-campo-dataCadastro').val(data.DataCadastro);
+                $('#clientePessoaJuridica-campo-cnpj').val(data.Cnpj);
+                $('#clientePessoaJuridica-campo-email').val(data.Email);
+                $('#clientePessoaJuridica-campo-filial').val(data.Filial);
+                $('#clientePessoaJuridica-campo-telefone').val(data.Telefone);
+                $('#clientePessoaJuridica-campo-cep').val(data.Cep);
+                $('#clientePessoaJuridica-campo-logradouro').val(data.Logradouro);
+                $('#clientePessoaJuridica-campo-numero').val(data.Numero);
+                $('#clientePessoaJuridica-campo-bairro').val(data.Bairro);
+                $('#clientePessoaJuridica-campo-uf').val(data.Uf);
+                $('#clientePessoaJuridica-campo-cidade').val(data.Cidade);
                 $('#modal-clientePessoaJuridica').modal('show');
             },
             error: function (err) {
