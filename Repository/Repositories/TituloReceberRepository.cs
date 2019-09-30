fd19a3a7c01887a57464a51425ca7466ed1a81f4
JoaoPstein <vulgostein@gmail.com>
2019-09-30 19:16:56 -0300

    Falta resolver erro da tabela titulo receber

--- Repository/Repositories/TituloReceberRepository.cs
+++ Repository/Repositories/TituloReceberRepository.cs
@@ -28,7 +28,6 @@ namespace Repository.Repositories
             tituloReceberOriginal.IdClientePessoaJuridica = tituloReceber.IdClientePessoaJuridica;
             tituloReceberOriginal.IdClientePessoaFisica = tituloReceber.IdClientePessoaFisica;
             tituloReceberOriginal.IdCategoriaReceita = tituloReceber.IdCategoriaReceita;
-
             tituloReceberOriginal.Descricao = tituloReceber.Descricao;
             tituloReceberOriginal.Status = tituloReceber.Status;
             tituloReceberOriginal.DataLancamento = tituloReceber.DataLancamento;
