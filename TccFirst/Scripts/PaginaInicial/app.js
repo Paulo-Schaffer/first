75f7d28947f03c2e2191a988371c84c59f87f2c9
AndreWenceslau <andrewenceslaudeoli@gmail.com>
2019-09-30 11:23:16 -0300

    arrumado js app

--- TccFirst/Scripts/PaginaInicial/app.js
+++ TccFirst/Scripts/PaginaInicial/app.js
@@ -1,14 +1,14 @@
 $('nav a').click(function (e) {
-	var id = $(this).attr('href'),
+    var id = $(this).attr('href'),
 
-    if (id == "/login") {
-        return;
-    }
-	e.preventDefault();
-	targetOffset = $(id).offset().top;
+    //if (id == "/login") {
+    //    return;
+    //}
+    //e.preventDefault();
+    targetOffset = $(id).offset().top;
 
-	$('html, body').animate({ 
-		scrollTop: targetOffset -100
-	  }, 500);
+    $('html, body').animate({
+        scrollTop: targetOffset - 100
+    }, 500);
 });
 
