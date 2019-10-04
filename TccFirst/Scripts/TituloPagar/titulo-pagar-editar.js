e1b4ac134553f039413d59d75e1b818c6a928316
JoaoPstein <vulgostein@gmail.com>
2019-10-04 09:13:05 -0300

    Sistema initializer certo.

--- TccFirst/Scripts/TituloPagar/titulo-pagar-editar.js
+++ TccFirst/Scripts/TituloPagar/titulo-pagar-editar.js
@@ -162,6 +162,9 @@ $(function () {
             url: '/parcelasPagar/obterpeloid?id=' + $idAlterar,
             method: 'get',
             success: function (data) {
+                if ($idTituloPagar.Status == "Pago") {
+                    $('.botao-editar') = false;
+                }
                 $('#parcelasPagar-campo-data-pagamento').val(data.DataPagamento);
                 $('#modal-parcelasPagar').modal('show');
             },
