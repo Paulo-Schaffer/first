02bf14d1c320b93e6c42a0a1b2a35fcd81a732b9
Paulo-Schaffer <paulo.md10@gmail.com>
2019-10-04 11:24:54 -0300

    Arrumando parcelas

--- TccFirst/Scripts/TituloReceber/titulo-receber-editar.js
+++ TccFirst/Scripts/TituloReceber/titulo-receber-editar.js
@@ -22,14 +22,14 @@
                     return moment(row.DataRecebimento).format('DD/MM/YYYY')
                 }
             },
-            
+
             { data: "Valor" },
             {
                 render: function (data, type, row) {
                     let cor = "";
                     if (row.Status == "Pago") {
                         cor = "bg-success";
-                    } else if (row.Status == "Pendente") {
+  x                    } else if (row.Status == "Pendente") {
                         cor = "bg-warning";
                     } else {
                         cor = "bg-danger";
@@ -37,12 +37,12 @@
                     return "<span class='" + cor + " pr-2 pl-2 b2-1 rounded'>" + row.Status + "</span>"
 
                 }
-            },          
+            },
             {
                 render: function (data, type, row) {
                     return "\
                     <button class='btn btn-primary botao-editar fa fa-edit'\
-                        data-id" + row.Id + "'\
+                        data-id" + row.Id + " id='botao-editar''\
                         data-id=" + row.Id + "> Editar</button>";
                     ;
                 }
@@ -111,10 +111,10 @@
             monstrarMensagem('Digite a Quantidade de Parcelas', '', 'error');
             $("#tituloReceber-campo-quantidade-Parcelas").focus();
             return false;
-        //} else if ($ValorTotal == '') {
-        //    monstrarMensagem('Digite a Data de Valor Total', '', 'error');
-        //    $("#tituloReceber-campo-valor-total").focus();
-        //    return false;
+            //} else if ($ValorTotal == '') {
+            //    monstrarMensagem('Digite a Data de Valor Total', '', 'error');
+            //    $("#tituloReceber-campo-valor-total").focus();
+            //    return false;
         } else if ($Descricao == '') {
             monstrarMensagem('Digite a Descrição', '', 'error');
             $("#tituloReceber-campo-descricao").focus();
@@ -125,10 +125,10 @@
     });
 
     $('#parcelasReceber-botao-salvar').on('click', function () {
-        
+
         $dataRecebimento = $('#parcelasReceber-campo-data-recebimento').val();
 
-       
+
         //if ($categoriaReceita == undefined) {
         //    monstrarMensagem('Digite o Nome', '', 'error');
         //    $('#clientePessoaFisica-campo-nome').focus();
@@ -176,6 +176,12 @@
                 $('#parcelasReceber-campo-data-recebimento').val(data.DataRecebimento);
                 $('#parcelasReceber-campo-status').val(data.Status);
                 $('#modal-parcelasReceber').modal('show');
+                if (row.Status == "Pago") {
+                    $('.botao-editar'.hide())
+                }
+                else {
+                    $('.botao-editar'.show())
+                }
             },
             error: function (err) {
                 alert('Não foi possível carregar');
